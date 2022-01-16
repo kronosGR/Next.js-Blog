070447f282c3c5e572fab2f233e3fa3e3bf81573
kronosGR <geo.elgeo@gmail.com>
2022-01-16 07:30:25 +0100

    put all together with dummy data

--- components/posts/posts-grid.js
+++ components/posts/posts-grid.js
@@ -1,12 +1,16 @@
-import PostItem from './post-item'
-import classes from './posts-grid.module.css'
+import PostItem from './post-item';
+import classes from './posts-grid.module.css';
 
-function PostGrid(props){
-  const {posts} = props
+function PostGrid(props) {
+  const { posts } = props;
 
-  return <ul className={classes.grid}>
-    {posts.map(post => <PostItem /> )}
-  </ul>
+  return (
+    <ul className={classes.grid}>
+      {posts.map((post) => (
+        <PostItem key={post.slug} post={post}/>
+      ))}
+    </ul>
+  );
 }
 
-export default PostGrid
+export default PostGrid;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,46 @@
-import { Fragment } from "react"
+import { Fragment } from 'react';
 
-import Hero from'../components/home-page/hero';
+import Hero from '../components/home-page/hero';
+import FeaturedPosts from '../components/home-page/featured-posts';
+
+const DUMYY_POSTS = [
+  {
+    slug: 'getting-started',
+    title: 'Getting started',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'lorem lorem lorem ipsum',
+    date:'2022-02-10',
+  },
+  {
+    slug: 'getting-started2',
+    title: 'Getting started2',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'lorem lorem lorem ipsum',
+    date:'2022-02-10',
+  },
+  {
+    slug: 'getting-started3',
+    title: 'Getting started3',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'lorem lorem lorem ipsum',
+    date:'2022-02-10',
+  },
+  {
+    slug: 'getting-started4',
+    title: 'Getting started4',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'lorem lorem lorem ipsum',
+    date:'2022-02-10',
+  },
+];
 
 function HomePage() {
-  return <Fragment>
-    <Hero/>
-    {/* <FeaturedPosts/> */}
-  </Fragment>
+  return (
+    <Fragment>
+      <Hero />
+      <FeaturedPosts posts={DUMYY_POSTS} />
+    </Fragment>
+  );
 }
 
-export default HomePage
-
+export default HomePage;
