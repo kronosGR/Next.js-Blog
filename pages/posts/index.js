e791dda83960fb14b2fd31e9b14694953290f0a8
kronosGR <geo.elgeo@gmail.com>
2022-01-16 07:50:51 +0100

    implemented allposts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,38 @@
-function AllPostsPage() {
+import AllPosts from "../../components/posts/all-posts"
 
+const DUMYY_POSTS = [
+  {
+    slug: 'getting-started',
+    title: 'Getting started',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'lorem lorem lorem ipsum',
+    date:'2022-02-10',
+  },
+  {
+    slug: 'getting-started2',
+    title: 'Getting started2',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'lorem lorem lorem ipsum',
+    date:'2022-02-10',
+  },
+  {
+    slug: 'getting-started3',
+    title: 'Getting started3',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'lorem lorem lorem ipsum',
+    date:'2022-02-10',
+  },
+  {
+    slug: 'getting-started4',
+    title: 'Getting started4',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'lorem lorem lorem ipsum',
+    date:'2022-02-10',
+  },
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMYY_POSTS}/>
 }
 
 export default AllPostsPage
