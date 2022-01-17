aff703f9ba0c405d6b94adc1e52adfcc34c93ba0
kronosGR <geo.elgeo@gmail.com>
2022-01-17 09:47:34 +0100

    added dynamic post rendering

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,22 +2,18 @@ import ReactMarkdown from 'react-markdown';
 
 import PostHeader from './post-header';
 
-import classes from './post-content.module.css'
+import classes from './post-content.module.css';
 
-const DUMMY_POST =  {
-  slug: 'getting-started4',
-  title: 'Getting started4',
-  image: 'getting-started-nextjs.png',
-  date:'2022-02-10',
-  content: '# This is a first post'
+function PostContent(props) {
+  const { post } = props;
+
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  return (
+    <article className={classes.content}>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
+    </article>
+  );
 }
 
-function PostContent() {
-  const imagePath =`/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
-  return <article className={classes.content}>
-    <PostHeader title={DUMMY_POST.title} image={imagePath} />
-    <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
-  </article>
-}
-
-export default PostContent
+export default PostContent;
