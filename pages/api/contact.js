4fc8bff7b377162587418ea77092498a9de495c4
kronosGR <geo.elgeo@gmail.com>
2022-01-20 08:09:26 +0100

    added mongodb for the contact page

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
@@ -20,9 +22,31 @@ function handler(req, res) {
       message,
     };
 
-    console.log(newMessage);
+    let client;
 
-    res.status(201).json({message: 'Successfully stored message!', message: newMessage});
+    try {
+     client = await MongoClient.connect(
+        'mongodb+srv://nextblog:4wYIY79oBgEEC6Up@cluster0.hrnez.mongodb.net/blog?retryWrites=true&w=majority'
+      );
+    } catch (error) {
+      res.status(500).json({ message: 'Could not connect to database' });
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage._id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: 'Storing message failed' });
+      return;
+    }
+
+    client.close();
+    res
+      .status(201)
+      .json({ message: 'Successfully stored message!', message: newMessage });
   }
 }
 
